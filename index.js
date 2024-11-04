d0b915c19edff2e818d05f744323958b37cd5221
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-11-04 21:07:11 +0530

    subhan

--- index.js
+++ index.js
@@ -53,9 +53,11 @@ app.post("/register", async (req, res) => {
             return res.status(400).json({
                 error: "Incorrect username or password"
             });
+        }else{
+            res.json("Logged in");
         }
         
-        res.json("Logged in");
+        
         
     } catch (error) {
         res.status(500).json({
