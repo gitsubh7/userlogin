d4f85ba65543850fb4eada840ea39dc654de8d76
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-11-05 21:37:34 +0530

    netlify added

--- functions/index.js
+++ functions/index.js
@@ -1,98 +1,100 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import db from '../models/db.js'; 
-import bcrypt from 'bcrypt'
-import User from '../models/user.js'
-import cookieParser from 'cookie-parser';
-import jwt from 'jsonwebtoken';
-import {createToken} from '../jwt.js';
-import { validateToken } from '../jwt.js';
+    import express from 'express';
+    import dotenv from 'dotenv';
+    import db from '../models/db.js'; 
+    import bcrypt from 'bcryptjs'
+    import User from '../models/user.js'
+    import cookieParser from 'cookie-parser';
+    import jwt from 'jsonwebtoken';
+    import serverless from 'serverless-http';
+    import {createToken} from '../jwt.js';
+    import { validateToken } from '../jwt.js';
 
-dotenv.config({
-    path:'./.env'
-});
-
-
-const app=express();
-const router=express.Router();
-app.use(express.json());
-app.use(cookieParser());
-
-router.post("/register", async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        return res.status(400).json({ message: "Username already exists." });
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const user = new User({
-        username,
-        password: hashedPassword
-      });
-      await user.save();
-      res.status(201).json({ message: "User registered successfully." });
-    } catch (error) {
-      console.error("Error in /register route:", error);
-      res.status(500).json({ message: "Internal server error." });
-    }
-  });
-
-
-
-  router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    
-    try {
-        const user = await User.findOne({ username: username });
-        if (!user) {
-            return res.status(400).json({
-                error: "User does not exist"
-            });
-        }
-        
-        const match = await bcrypt.compare(password, user.password);
-        
-        if (!match) {
-            return res.status(400).json({
-                error: "Incorrect username or password"
-            });
-        }else{
-            const accessToken=createToken(user);
-            res.cookie("access-token",accessToken,{
-                maxAge:60*60*24*30*1000
-            })
-            res.json("Logged in");
-        }
-        
-        
-        
-    } catch (error) {
-        res.status(500).json({
-            error: "An error occurred during login"
-        });
-    }
-});
-
-router.get('/profile',validateToken,(req,res)=>{
-    res.json("blah blah blah"); 
-});
-
-db()
-    .then(()=>{
-        app.listen(process.env.PORT || 8000, ()=>{
-            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-        });
-    })
-    .catch((err)=>{
-        console.log("Mongo DB connection failed!!!",err);
+    dotenv.config({
+        path:'./.env'
     });
 
-app.use('/.netlify/functions/index',router);
 
-module.exports.handler=serverless(app);
+    const app=express();
+    const router=express.Router();
+    app.use(express.json());
+    app.use(cookieParser());
+
+    router.post("/register", async (req, res) => {
+        try {
+        const { username, password } = req.body;
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "Username already exists." });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        const user = new User({
+            username,
+            password: hashedPassword
+        });
+        await user.save();
+        res.status(201).json({ message: "User registered successfully." });
+        } catch (error) {
+        console.error("Error in /register route:", error);
+        res.status(500).json({ message: "Internal server error." });
+        }
+    });
+
+
+
+    router.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+        
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return res.status(400).json({
+                    error: "User does not exist"
+                });
+            }
+            
+            const match = await bcrypt.compare(password, user.password);
+            
+            if (!match) {
+                return res.status(400).json({
+                    error: "Incorrect username or password"
+                });
+            }else{
+                const accessToken=createToken(user);
+                res.cookie("access-token",accessToken,{
+                    maxAge:60*60*24*30*1000
+                })
+                res.json("Logged in");
+            }
+            
+            
+            
+        } catch (error) {
+            res.status(500).json({
+                error: "An error occurred during login"
+            });
+        }
+    });
+
+    router.get('/profile',validateToken,(req,res)=>{
+        res.json("blah blah blah"); 
+    });
+
+    db()
+        .then(()=>{
+            app.listen(process.env.PORT || 8000, ()=>{
+                console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+            });
+        })
+        .catch((err)=>{
+            console.log("Mongo DB connection failed!!!",err);
+        });
+
+    app.use('/.netlify/functions/index',router);
+
+    // export const handler = serverless(app);
+    export {app};
 
 
 
